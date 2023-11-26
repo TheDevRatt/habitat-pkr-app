93ab16642b8a0e87151c8ad4d50a531e10442876
MattMakary <matthewmakary@trentu.ca>
2023-11-26 16:21:27 -0500

    Figured out SVG's
    Got the SVG component to display so that is absolutely fantastic.

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,16 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Image } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 
+import PKRLogo from "@/components/PKRLogo";
+
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Index</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+    <View className="flex-1 items-center justify-center bg-white">
+      <PKRLogo />
+      <Text>Index</Text>
       <EditScreenInfo path="app/index.tsx" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
