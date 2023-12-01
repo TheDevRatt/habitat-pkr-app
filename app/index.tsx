b169ea4fa3718062cf060318242dcfcabf2ebdc6
emmavie <75275724+emmavie@users.noreply.github.com>
2023-12-01 11:08:21 -0500

    small changes

--- app/index.tsx
+++ app/index.tsx
@@ -8,6 +8,7 @@ import PKRLogo from "../components/PKRLogo";
 import AppButton from "../components/AppButton"; // Import the Button component
 import DividerWithText from "../components/DividerWithText";
 import {Link, useRouter} from 'expo-router'
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function HomeScreen() {
 const router = useRouter();
@@ -28,6 +29,7 @@ const router = useRouter();
         className="flex-initial mt-[29px] w-[350px] h-[55px] justify-center"
         onPress={() => router.push('/onboarding/logIn')}
       >
+   
         Log In
       </AppButton>
 
@@ -41,21 +43,25 @@ const router = useRouter();
       <DividerWithText />
 
       <View className="flex-row space-x-10 bg-transparent">
+
         <AppButton
           className="flex-initial w-[160px] h-[55px] justify-center"
-          onPress={() => console.log("Sign Up Button Pressed")}
+          onPress={() => console.log("Google Button Pressed")}
         >
-          <Text></Text>
-          Google
+           {/* <FontAwesome className="mr-4" name={"google"} size={25} /> */}
+          <Text className="ml-4" >Google</Text>
+          
         </AppButton>
 
         <AppButton
           className="flex-initial w-[160px] h-[55px] justify-center"
-          onPress={() => console.log("Sign Up Button Pressed")}
+          onPress={() => console.log("Apple Button Pressed")}
         >
+           {/* <FontAwesome className="mr-4" name={"apple"} size={25} /> */}
           Apple
         </AppButton>
       </View>
+    
     </LinearGradient>
   );
 }
