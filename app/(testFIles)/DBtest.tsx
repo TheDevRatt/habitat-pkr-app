8e4fbf3a73d0bcfac648beab5e64de44569e06e8
Brady Gibson <bradydjg@hotmail.com>
2024-01-20 20:18:54 -0500

    Testing Realm and Atlas

---
+++ app/(testFIles)/DBtest.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {RealmProvider} from '@realm/react';
+// Import your models
+import {Profile} from '../../../models';
+export const AppWrapper = () => {
+  return (
+    <RealmProvider schema={[Profile]}>
+      <RestOfApp />
+    </RealmProvider>
+  );
+};
