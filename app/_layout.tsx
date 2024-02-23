757295eeef5756f7e336f5bf86a69ffbdd45f888
Blinky <matthewmakary@trentu.ca>
2024-02-23 16:22:36 -0500

    Modifications
    - Edited the layout.tsx file
    - Added the InsuranceLogo.svg
    - Edited themed to have touchableopacity.
    - Added the appbutton.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,24 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { useColorScheme } from "@/components/components(old)/useColorScheme";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "index",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -22,8 +26,20 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    karlaB: require("../assets/fonts/Karla-Bold.ttf"),
+    karlaBI: require("../assets/fonts/Karla-BoldItalic.ttf"),
+    karlaEB: require("../assets/fonts/Karla-ExtraBold.ttf"),
+    karlaEBI: require("../assets/fonts/Karla-ExtraBoldItalic.ttf"),
+    karlaEL: require("../assets/fonts/Karla-ExtraLight.ttf"),
+    karlaELI: require("../assets/fonts/Karla-ExtraLightItalic.ttf"),
+    karlaI: require("../assets/fonts/Karla-Italic.ttf"),
+    karlaL: require("../assets/fonts/Karla-Light.ttf"),
+    karlaLI: require("../assets/fonts/Karla-LightItalic.ttf"),
+    karlaM: require("../assets/fonts/Karla-Medium.ttf"),
+    karlaMI: require("../assets/fonts/Karla-MediumItalic.ttf"),
+    karlaR: require("../assets/fonts/Karla-Regular.ttf"),
+    karlaSB: require("../assets/fonts/Karla-SemiBold.ttf"),
+    karlaSBI: require("../assets/fonts/Karla-SemiBoldItalic.ttf"),
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -45,14 +61,33 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    // <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="onboarding/signUp" options={{ headerShown: false }} />
+      <Stack.Screen name="onboarding/logIn" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="onboarding/basicInfo"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="onboarding/membership"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="onboarding/paymentInfo"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="onboarding/TermsAndConditions"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+    </Stack>
+    // </ThemeProvider>
   );
 }
