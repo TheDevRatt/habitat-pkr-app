0a6653bd6877ef340f927340b5b2e7480918fa32
Blinky <matthewmakary@trentu.ca>
2024-03-10 16:29:26 -0400

    Fixed link to Forgot Password

--- app/onboarding/logIn.tsx
+++ app/onboarding/logIn.tsx
@@ -42,7 +42,7 @@ const LogIn = () => {
   const handleForgotPasswordPress = () => {
     console.log("Navigating to Forgot Password Page");
     // Once forgot password is added this error should go away.
-    router.push("/onboarding/forgotPassword");
+    router.push("/onboarding/ForgotPassword");
   };
 
   const handleCreateAccountPress = () => {
